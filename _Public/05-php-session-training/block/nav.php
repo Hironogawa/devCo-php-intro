b9b13329820c414ddcb94591c6949417ef845af8
Hironogawa <hello@hironogawa.com>
2023-07-25 11:27:48 +0200

    removed handout folder

--- _Public/05-php-session-training/block/nav.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="nav-link" href="index.php">Home</a>
-  <a class="nav-link" href="about.php">About</a>
-  <a class="nav-link" href="result.php">Result</a>
-</nav>
